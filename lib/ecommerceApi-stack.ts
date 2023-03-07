2ac84e8610b7dce5eaba79c950c2652ceaf84bdd
Ariel dos Santos Nunes <arielsn@outlook.com>
2023-03-07 20:00:28 -0300

    Criação de stack para o api gateway

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -2,15 +2,28 @@ import * as cwlogs from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AccessLogFormat, LambdaIntegration, LogGroupLogDestination, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
 
 interface EcommerceApiStackProps extends StackProps {
 	productsFetchHandler: NodejsFunction
 }
 
 export class EcommerceApiStack extends Stack {
+
+	private readonly logGroup: LogGroup;
+
 	constructor(scope: Construct, id: string, readonly props: EcommerceApiStackProps) {
 		super(scope, id, props);
+
+		/**
+		 * Cria um grupo de logs
+		 */
+		this.logGroup = new LogGroup(this, 'EcommerceApiLogs');
+
+		/**
+		 * Cria o api gateway
+		 */
 		this.createApiGateway.call(this);
 	}
 
@@ -21,13 +34,34 @@ export class EcommerceApiStack extends Stack {
 		const functionName = 'ProductsFetchFunction';
 
 		/**
+		 * Cria o destino do log de grupos
+		 */
+		const accessLogDestination = new LogGroupLogDestination(this.logGroup);
+
+		/**
+		 * Formato o log
+		 */
+		const accessLogFormat = AccessLogFormat.jsonWithStandardFields({
+			httpMethod: true,
+			requestTime: true,
+			ip: true,
+			protocol: true,
+			resourcePath: true,
+			responseLength: true,
+			status: true,
+			user: false,
+			caller: true
+		});
+
+		/**
 		 * Cria o api gateway
 		 */
 		const api = new RestApi(
 			this,
 			functionName,
 			{
-				restApiName: functionName
+				restApiName: functionName,
+				deployOptions: { accessLogDestination, accessLogFormat }
 			}
 		);
 
