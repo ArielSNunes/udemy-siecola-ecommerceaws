5a7a319edbf4ce59045e0ea39b90c8aef7a63971
Ariel dos Santos Nunes <arielsn@outlook.com>
2023-03-20 19:48:12 -0300

    Criação do lambda layer

---
+++ lib/productsAppLayers-stack.ts
@@ -0,0 +1,48 @@
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Code, LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
+import { Construct } from "constructs";
+
+export class ProductAppLayersStack extends Stack {
+	readonly productsLayer: LayerVersion;
+
+	constructor(
+		private readonly scope: Construct,
+		private readonly id: string,
+		private readonly props?: StackProps
+	) {
+		super(scope, id, props);
+
+		this.productsLayer = this.createLayerVersion.call(this);
+	}
+
+	createLayerVersion() {
+		const layerName = 'ProductsLayer';
+
+		const systemManagerName = 'ProductsLayerVersionARN';
+
+		const lambdaLayer = new LayerVersion(
+			this,
+			layerName,
+			{
+				code: Code.fromAsset('lambda/products/layers/productsLayer'),
+				compatibleRuntimes: [
+					Runtime.NODEJS_14_X
+				],
+				layerVersionName: layerName,
+				removalPolicy: RemovalPolicy.RETAIN
+			}
+		);
+
+		const systemManager = new StringParameter(
+			this.scope,
+			systemManagerName,
+			{
+				parameterName: systemManagerName,
+				stringValue: this.productsLayer.layerVersionArn
+			}
+		);
+
+		return lambdaLayer;
+	}
+}
